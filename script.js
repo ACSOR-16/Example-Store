9da3dd4d77a1e65bfb6ee624cb9087089784f82e
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-02-09 11:52:43 -0500

    arreglando el despliegue de menus

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamburguerIcon = document.querySelector(".menu");
+const menuShopCar = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const myOrder = document.querySelector(".product-detail");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamburguerIcon.addEventListener('click', toggleMobileMenu)
+menuHamburguerIcon.addEventListener('click', toggleMobileMenu);
+menuShopCar.addEventListener('click', toggleMyOrder);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle("inactive")
+  const isMyOrderClosed = myOrder.classList.contains('inactive');
+
+  if (!isMyOrderClosed) {
+    myOrder.classList.add('inactive');
+  }
+
+  desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle("inactive")
+  const isMyOrderClosed = myOrder.classList.contains('inactive');
+
+  if (!isMyOrderClosed) {
+    myOrder.classList.add('inactive');
+  }
+  mobileMenu.classList.toggle("inactive");
 }
+
+function toggleMyOrder() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  myOrder.classList.toggle("inactive");  
+} 
